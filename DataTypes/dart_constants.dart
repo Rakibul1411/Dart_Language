61ad1753fed2d9e540409294c58e5b08f61f3fab
Rakibul1411 <bsse1411@iit.du.ac.bd>
2024-09-26 23:08:07 +0600

    initial commit

---
+++ DataTypes/dart_constants.dart
@@ -0,0 +1,12 @@
+void main() {
+  var thisYear = 2024;
+  thisYear = 2025;
+  print(thisYear);
+
+  // for declaration constant variables
+  const year = 2024;
+  print(year);
+
+  final month = 9;
+  print(month);
+}
---
+++ ListProperties/List_properties.dart
@@ -0,0 +1,22 @@
+void main() {
+  var city = ['Dhaka', 'Khulna', 'Kushtia'];
+
+  var result = city.length;
+  print(result);
+
+  var cityName = city.first;
+  print(cityName);
+
+  cityName = city.last;
+  print(cityName);
+
+  var cityNames = city.reversed;
+  print(cityNames);
+
+  var check = city.isEmpty;
+  print(check);
+
+  check = city.isNotEmpty;
+  print(check);
+  
+}
