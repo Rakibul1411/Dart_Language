65a9ec15cf838a97023254b740bfb4a0f5d2c4e3
Rakibul1411 <bsse1411@iit.du.ac.bd>
2024-09-26 22:43:59 +0600

    initial commit

---
+++ maps.dart
@@ -0,0 +1,12 @@
+void main() {
+  var student = {
+    'name': "Rakibul",
+    'hall': 'AEH',
+    'branch': 'IIT'
+  };
+
+  print("$student\n");
+  print(student['hall']);
+  print("\n");
+  print(student['hal']);
+}
