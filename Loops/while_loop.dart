fce8e151c669a49b6b8403c25ecb8c9ef40ce652
Rakibul1411 <bsse1411@iit.du.ac.bd>
2024-09-27 09:41:09 +0600

    initial commit

---
+++ Loops/while_loop.dart
@@ -0,0 +1,16 @@
+void main() {
+  var i = 0;
+  while(i < 10){
+    print('Value: ' + i.toString());
+    i++;
+  }
+
+  print("\n");
+
+  var j = 0;
+  do {
+    print(j);
+    j++;
+  } while(j < 10);
+
+}
